4f3df8584dce91bb5dcc70bf48a29fa6b2554ea4
Jaskarn <jaskarn15@gmail.com>
2022-04-05 01:16:47 +0000

    added template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return "✅✅✅ Asseration Passed: " + actual + " === " + expected;
+    return `✅✅✅ Asseration Passed: ${actual} === ${expected}`;
   } else {
-    return "❌❌❌ Asseration Failed: " + actual + " !== " + expected;
+    return `❌❌❌ Asseration Failed: ${actual} !== ${expected}`;
   }
 };
 
