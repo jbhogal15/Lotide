8ca74174f881a2c91703cae108c130bcd85ce3b9
Jaskarn <jaskarn15@gmail.com>
2022-04-08 09:02:28 +0000

    find key value with cb

---
+++ findKey.js
@@ -0,0 +1,38 @@
+//Assert Equal
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Asseration Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌❌❌ Asseration Failed: ${actual} !== ${expected}`;
+  }
+};
+
+
+
+
+//Actual Function
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+//Test Cases
+let obj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+console.log(assertEqual(findKey(obj, x => x.stars === 2), "noma"));
+console.log(assertEqual(findKey(obj, x => x.stars === 3), "Akaleri"));
+console.log(assertEqual(findKey(obj, x => x.stars === 4), undefined));
+
