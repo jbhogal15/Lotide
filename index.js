cca1f9bc19b7a63adb2ad3799a0cf60de3e2be8b
Jaskarn <jaskarn15@gmail.com>
2022-04-12 03:43:03 +0000

    index of all functions of lotide

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArrayEqual = require('./assertArrayEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArray = require('./eqArray');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArrayEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArray,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
+
+
