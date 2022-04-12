18c934597b8f516afe5e436d7f2e9b100392d4f9
Jaskarn <jaskarn15@gmail.com>
2022-04-12 03:46:47 +0000

    refactored assertArrayEqual function

---
+++ assertArrayEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = require("./eqArray");
+
+
+
+
+
+
+// assertArrayEqual
+const assertArrayEqual = function(arr1, arr2) {
+  //If statement will allow to call the eqArray function on the parameters to check if it evaluated to true
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Asseration Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Asseration Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+module.exports = assertArrayEqual;
+
+//console.log(assertArrayEqual([1, 2, 3], [1, 2, 3]));
+
